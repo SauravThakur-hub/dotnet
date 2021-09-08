f87969f774ea53dec38049a567bfe4204e8b4c0e
saurav.thakur <saurav.thakur@oneadvanced.com>
2021-09-08 10:55:08 +0530

    modified

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace _net
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello ");
         }
     }
 }
